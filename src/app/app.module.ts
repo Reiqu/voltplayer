388d1aad730d2f2a2f6bd4aca09bb00c7d56ffce
Adrian <reiqu@outlook.de>
2018-08-04 22:05:05 +0200

    updated declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PlayerComponent } from './player/player.component';
+import { SidelistComponent } from './sidelist/sidelist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayerComponent,
+    SidelistComponent
   ],
   imports: [
     BrowserModule
